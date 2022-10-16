302e1580942cf6ccda3b5ccb583f2a71ca08ead9
David Falson <david@falson.net>
2022-10-16 11:53:34 -0700

    renamed function and commented out broken code

--- wordfinder.py
+++ wordfinder.py
@@ -9,7 +9,7 @@ class WordFinder:
         self.word_count = self.parse(word_file)
         print(self.word_count, " words read")
     
-    def parse(self, word_file):
+    def count(self, word_file):
         for line in word_file:
             self.word_count += 1
         return self.word_count
@@ -19,15 +19,23 @@ class WordFinder:
         return choice(self.file)
        
 
+
+# 
 class SpecialWordFinder:
     def __init__(self,path):
         super().__init__(self,path)
+    
+    def count(self, word_file):
+        for line in word_file:
+            self.word_count += 1
+        return self.word_count
 
-    def random(self):
-        super.random(self)
-        while True:
-            if self.random_choice.startswith('\n') or self.random_choice.startswith('#'):
-                self.random_choice = choice(self.file)
-            break
-        print(self.random_choice)
-        
+    
+    # def random(self):
+    #     super.random(self)
+    #     while True:
+    #         if self.random_choice.startswith('\n') or self.random_choice.startswith('#'):
+    #             self.random_choice = choice(self.file)
+    #         break
+    #     print(self.random_choice)
+    
